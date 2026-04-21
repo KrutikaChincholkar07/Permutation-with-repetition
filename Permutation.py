a030b0bbd617da98d0363f75ac7ff096d8210942
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2026-04-21 23:54:44 +0530

    Add permutation with repetition program
    Example:
    If there are 3 digits (0,1,2) and we need 2 positions, the total permutations are:
    
    3**2=9
    
    This repository helps beginners understand how to implement the permutation with repetition formula using programming.

---
+++ Permutation.py
@@ -0,0 +1,8 @@
+# Permutation with repetition program
+
+n = int(input("Enter number of items (n): "))
+r = int(input("Enter number of positions (r): "))
+
+result = n ** r
+
+print("Permutation with repetition =", result)
